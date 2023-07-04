d6fc5940536cf1df1d0f616bb238916153e17bb7
paulynson <paulabakada@gmail.com>
2023-07-05 00:16:12 +0100

    ignore react-quill

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Notes from "./components/Notes";
 
 export default function HomePage() {
   return (
-    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
     <main className="min-h-screen">
       <div className="mt-24">
         {/* @ts-expect-error Server Component */}
